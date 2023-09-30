3e36ce9e63dcd1c74587c0ef690e678aae4f2a7c
Yader Velasquez <yajosev@gmail.com>
2023-09-29 20:26:20 -0600

    Get zoom recording list

--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 import base64
 import os
-from urllib.parse import urlencode
+from urllib.parse import urlencode, urljoin
 
 import requests
 from fastapi import FastAPI
@@ -9,6 +9,8 @@ app = FastAPI()
 
 AUTH_URL = "https://zoom.us/oauth/authorize"
 TOKEN_URL = "https://zoom.us/oauth/token"
+API_URL = "https://api.zoom.us/"
+U_REC_LIST_ENDPOINT = "/v2/users/me/recordings" # user recording list
 
 @app.get("/zoom/url")
 def zoom_url():
@@ -42,3 +44,18 @@ def zoom_connect(code: str):
     }
     return requests.post(TOKEN_URL, auth=auth, data=data).json()
 
+@app.get("/zoom/list")
+def zomm_list(meeting_id: str = "", from_date: str = "", to_date: str = ""):
+    zoom_token = os.getenv("ZOOM_TOKEN", "")
+    if zoom_token == "":
+        return {"Error": "No ZOOM_TOKEN found in .env file"}
+    base_url = urljoin(API_URL, U_REC_LIST_ENDPOINT)
+    params = {}
+    if meeting_id != "":
+        params["meeting_id"] = meeting_id
+    if from_date != "":
+        params["from_date"] = from_date
+    if to_date != "":
+        params["to_date"] = to_date
+    url = f"{base_url}?{urlencode(params)}"
+    return requests.get(url, headers={"Authorization": f"Bearer {zoom_token}"}).json()
